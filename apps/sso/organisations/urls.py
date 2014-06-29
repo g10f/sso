5a676c7993e5b318232de829547fb830840060cc
gunnar <gunnar@g10f.de>
2014-06-29 17:45:28 +0200

    Added MyOrganisationsDetailView

--- apps/sso/organisations/urls.py
+++ apps/sso/organisations/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
 import views
  
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', views.OrganisationList.as_view(), name='organisation_list'), 
+    url(r'^me/$', views.MyOrganisationDetailView.as_view(), name='my_organisation_detail'),
     url(r'^(?P<uuid>[a-z0-9]{32})/$', views.OrganisationDetailView.as_view(), name='organisation_detail'),
     url(r'^(?P<uuid>[a-z0-9]{32})/update/$', views.OrganisationUpdateView.as_view(), name='organisation_update'),
     url(r'^(?P<uuid>[a-z0-9]{32})/delete/$', views.OrganisationDeleteView.as_view(), name='organisation_delete'),
