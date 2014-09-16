16ebbd972402d7c578d54091382d384c780a96c5
gunnar <gunnar@g10f.de>
2014-09-16 23:09:14 +0200

    openid connect session management

--- apps/sso/oauth2/decorators.py
+++ apps/sso/oauth2/decorators.py
@@ -1,85 +1,10 @@
 # -*- coding: utf-8 -*-
-try:
-    from urllib.parse import urlparse, urlunparse, urlsplit, urlunsplit
-except ImportError:     # Python 2
-    from urlparse import urlparse, urlunparse, urlsplit, urlunsplit
-    
 from functools import wraps
 from django.utils.decorators import available_attrs
 from django.core.exceptions import PermissionDenied
-from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.conf import settings
-from django.utils.encoding import force_str
-from django.shortcuts import resolve_url
-from django.utils import timezone
 
 import logging
-
- 
 logger = logging.getLogger(__name__)
-from django.http import HttpResponseRedirect, QueryDict
-
-def extract_query_param(url, attr):    
-    (scheme, netloc, path, query, fragment) = urlsplit(url)
-    query_dict = QueryDict(query).copy()
-    # get the last value 
-    value = query_dict.get(attr, None) 
-    if value:
-        del query_dict[attr]
-    query = query_dict.urlencode()
-    return urlunsplit((scheme, netloc, path, query, fragment)), value
-
-
-def redirect_to_login_and_extract_display_param(next, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME):  # @ReservedAssignment
-    """
-    Redirects the user to the login page, passing the given 'next' page
-    if there is a display parameter in the url we extract this parameter from the next url
-    and pass it directly to the login url
-    """
-    resolved_url = resolve_url(login_url or settings.LOGIN_URL)
-    
-    next, display = extract_query_param(next, 'display')  # @ReservedAssignment to next
-
-    login_url_parts = list(urlparse(resolved_url))
-    if redirect_field_name:
-        querystring = QueryDict(login_url_parts[4], mutable=True)
-        querystring[redirect_field_name] = next
-        if display:
-            querystring['display'] = display
-            
-        login_url_parts[4] = querystring.urlencode(safe='/')
-
-    return HttpResponseRedirect(urlunparse(login_url_parts))
-
-
-def login_required(view_func):
-    """
-    Copy of django.contrib.auth.decorators.login_required
-    with the following changes:
-    1. If there is a display parameter in the url, this parameter is extracted and 
-    gets passed to the login url. 
-    The display parameter changes the layout of the login form
-    2. if there is a max_age param check ...
-    """
-
-    @wraps(view_func, assigned=available_attrs(view_func))
-    def _wrapped_view(request, *args, **kwargs):
-        if request.user.is_authenticated():
-            max_age = request.POST.get('max_age') if request.POST.get('max_age') else request.GET.get('max_age')
-            if not max_age or (int(max_age) > (timezone.now() - request.user.last_login).total_seconds()):
-                return view_func(request, *args, **kwargs)
-        
-        path = request.build_absolute_uri()
-        # urlparse chokes on lazy objects in Python 3, force to str
-        resolved_login_url = force_str(resolve_url(settings.LOGIN_URL))
-        # If the login url is the same scheme and net location then just
-        # use the path as the "next" url.
-        login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
-        current_scheme, current_netloc = urlparse(path)[:2]
-        if ((not login_scheme or login_scheme == current_scheme) and (not login_netloc or login_netloc == current_netloc)):
-            path = request.get_full_path()
-        return redirect_to_login_and_extract_display_param(path, resolved_login_url)
-    return _wrapped_view
 
 
 def request_passes_test(test_func):
--- apps/sso/oauth2/views.py
+++ apps/sso/oauth2/views.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 import json
 import base64
-from django.views.decorators.cache import never_cache, cache_page
+try:
+    from urllib.parse import urlparse, urlunparse, urlsplit, urlunsplit
+except ImportError:     # Python 2
+    from urlparse import urlparse, urlunparse, urlsplit, urlunsplit
+from django.views.decorators.cache import never_cache, cache_page, cache_control
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.clickjacking import xframe_options_exempt
 from django.views.generic import TemplateView
+from django.utils import timezone
+from django.utils.decorators import method_decorator
+from django.utils.encoding import force_str
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse, QueryDict
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import permission_required, login_required
+from django.shortcuts import render, get_object_or_404, resolve_url
 from oauthlib import oauth2
 from oauthlib.common import urlencode
-
+from sso.api.decorators import condition
 from http.http_status import *  # @UnusedWildImport
-from sso.oauth2.decorators import login_required
 from utils.url import base_url
 from utils.convert import pack_bigint
 from sso.api.response import JsonHttpResponse 
@@ -37,6 +46,22 @@ def extract_params(request):
         headers['Authorization'] = headers['HTTP_AUTHORIZATION']
     body = urlencode(request.POST.items())
     return uri, http_method, body, headers
+
+
+def pop_query_param(url, param_name):  
+    """
+    get the query param with the param_name from the url and
+    return a new url without that param_name and
+    return the value of the param_name 
+    """  
+    (scheme, netloc, path, query, fragment) = urlsplit(url)
+    query_dict = QueryDict(query).copy()
+    # get the last value 
+    value = query_dict.get(param_name, None) 
+    if value:
+        del query_dict[param_name]
+    query = query_dict.urlencode()
+    return urlunsplit((scheme, netloc, path, query, fragment)), value
 
 
 class HttpOAuth2ResponseRedirect(HttpResponseRedirect):
@@ -79,15 +104,79 @@ def openid_configuration(request):
             ["page", "popup"],
         "service_documentation":
             "https://wiki.dwbn.org/general/AccessAndIdentityManagement",
-        "logout_uri": '%s%s' % (base_uri, reverse('accounts:logout')),
+        "end_session_endpoint": '%s%s' % (base_uri, reverse('accounts:logout')),
+        "check_session_iframe": '%s%s' % (base_uri, reverse('oauth2:session')),
         "certs_uri": '%s%s' % (base_uri, reverse('oauth2:certs')),
         "profile_uri": '%s%s' % (base_uri, reverse('accounts:profile'))
     }
     return JsonHttpResponse(configuration, request)
-    
 
-@login_required
+
+def get_last_modified_and_etag_for_me(request, *args, **kwargs):
+    if request.user.is_authenticated():
+        last_modified = request.user.last_login
+        etag = "%s/%s" % (request.user.uuid, last_modified)
+        return last_modified, etag
+    else:
+        return None, None
+
+
+class SessionView(TemplateView):
+    # TODO: enable caching
+    # @method_decorator(condition(last_modified_and_etag_func=get_last_modified_and_etag_for_me))
+    # @method_decorator(cache_control(must_revalidate=True, max_age=60 * 5)) 
+    @method_decorator(xframe_options_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(SessionView, self).dispatch(request, *args, **kwargs)       
+
+    def get_context_data(self, **kwargs):
+        context = super(SessionView, self).get_context_data(**kwargs)
+        context['session_cookie_name'] = settings.SESSION_COOKIE_NAME
+        return context
+
+
+class LoginRequiredError(oauth2.OAuth2Error):
+    error = 'login_required'
+    description = 'The End User is currently not already authenticated.'
+
+
+def get_param(request, name, default=None):
+    return request.POST.get(name) if request.POST.get(name) else request.GET.get(name, default)
+
+
+def redirect_to_login(request, redirect_field_name=REDIRECT_FIELD_NAME):  # @ReservedAssignment
+    """
+    Redirects the user to the login page, passing the given 'next' page
+    if there is a display parameter in the url we extract this parameter from the next url
+    and pass it directly to the login url
+    """
+    resolved_login_url = force_str(resolve_url(settings.LOGIN_URL))
+    full_path = request.get_full_path()
+    redirect_url, display = pop_query_param(full_path, 'display')
+
+    login_url_parts = list(urlparse(resolved_login_url))
+    if redirect_field_name:
+        querystring = QueryDict(login_url_parts[4], mutable=True)
+        querystring[redirect_field_name] = redirect_url
+        if display:
+            querystring['display'] = display
+            
+        login_url_parts[4] = querystring.urlencode(safe='/')
+
+    return HttpResponseRedirect(urlunparse(login_url_parts))
+
+
+import hashlib
+from django.utils.crypto import get_random_string
+def get_session_state(client_id, browser_state):
+    salt = get_random_string()
+    if browser_state is None:
+        browser_state = ""
+    return hashlib.sha256((client_id + " " + browser_state + " " + salt).encode('utf-8')).hexdigest() + "." + salt
+
+
 @never_cache
+@xframe_options_exempt
 def authorize(request):
     uri, http_method, body, headers = extract_params(request)
     error_uri = reverse('oauth2:oauth2_error')
@@ -95,8 +184,31 @@ def authorize(request):
     try:
         scopes, credentials = server.validate_authorization_request(uri, http_method, body, headers)
         credentials['user'] = request.user
+        credentials['session_state'] = get_session_state(credentials['client_id'], browser_state=request.session.session_key)
         credentials['client'] = credentials['request'].client
         redirect_uri = credentials.get('redirect_uri')
+        prompt = get_param(request, 'prompt', '').split()
+        
+        # check if the user must login
+        login_required = True
+        if request.user.is_authenticated() and ('login' not in prompt):
+            max_age = get_param(request, 'max_age')
+            if not max_age or (int(max_age) > (timezone.now() - request.user.last_login).total_seconds()):
+                login_required = False
+        
+        if 'none' in prompt:
+            if login_required:
+                raise LoginRequiredError(state=credentials.get('state'))
+            else:
+                id_token = get_param(request, 'id_token_hint', '')
+                parsed = loads_jwt(id_token)
+                if parsed['sub'] != request.user.uuid:
+                    raise LoginRequiredError(state=credentials.get('state'))
+                
+        if login_required: 
+            return redirect_to_login(request)
+            
+        # if we are here, the user is already logged in does not need to login again          
         headers, body, status = server.create_authorization_response(uri, http_method, body, headers, scopes, credentials)  # @UnusedVariable
         return HttpOAuth2ResponseRedirect(headers['Location'])
     except oauth2.FatalClientError as e:
