36b404bcf80eb5b1cebe1a4421c51343ac2bce4f
gunnar <gunnar@g10f.de>
2014-10-10 10:35:18 +0200

    add User AdminForm password validation

--- apps/sso/accounts/forms.py
+++ apps/sso/accounts/forms.py
@@ -173,9 +173,10 @@ class UserAddForm(forms.ModelForm):
     def clean_password2(self):
         password1 = self.cleaned_data.get("password1", "")
         password2 = self.cleaned_data["password2"]
-        if password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'])
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'])
         return password2
 
     def save(self, commit=True):
