72c2abda66e2c294f3af777f085ae03e41c9b188
gunnar <gunnar@g10f.de>
2014-11-01 23:01:57 +0100

    bootstrap 3.3

--- apps/sso/admin/__init__.py
+++ apps/sso/admin/__init__.py
@@ -25,8 +25,8 @@ class SSOAdminSite(admin.AdminSite):
 sso_admin_site = SSOAdminSite()
 
 sso_admin_site.register(Group, GroupAdmin)
-sso_admin_site.register(Permission, PermissionAdmin)
-sso_admin_site.register(ContentType)
+# sso_admin_site.register(Permission, PermissionAdmin)
+# sso_admin_site.register(ContentType)
 
 sso_admin_site.register(get_user_model(), UserAdmin)
 sso_admin_site.register(ApplicationRole, ApplicationRoleAdmin)
