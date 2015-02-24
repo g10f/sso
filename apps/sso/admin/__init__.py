9a5298b9134ceab59ef509e5a491a45ff48e6a6c
gunnar <gunnar@g10f.de>
2015-02-24 22:24:48 +0100

    admin login redirect to accounts:login

--- apps/sso/admin/__init__.py
+++ apps/sso/admin/__init__.py
@@ -1,23 +1,51 @@
-# -*- coding: utf-8 -*-
-from sso.auth.forms import EmailAuthenticationForm
-
-from sso.accounts.admin import *
-from sso.oauth2.admin import *
-from sso.registration.admin import *
+from functools import update_wrapper
+from django.contrib.auth import get_user_model
+from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.cache import never_cache
+from sso.accounts.admin import ApplicationAdmin, ApplicationAdminAdmin, ApplicationRoleAdmin, GroupAdmin, RoleAdmin, \
+    OneTimeMessageAdmin, OrganisationChangeAdmin, RoleProfileAdmin, RoleProfileAdminAdmin, UserAdmin, UserEmailAdmin
+from sso.oauth2.admin import ClientAdmin, AuthorizationCodeAdmin, BearerTokenAdmin, RefreshTokenAdmin
+from sso.oauth2.models import Client, AuthorizationCode, BearerToken, RefreshToken
+from sso.registration.admin import RegistrationAdmin
+from sso.registration.models import RegistrationProfile
+from sso.emails.models import Email, EmailForward, EmailAlias, GroupEmail, GroupEmailManager
+from sso.emails.admin import EmailAdmin, EmailAliasAdmin, EmailForwardAdmin, GroupEmailAdmin, GroupEmailManagerAdmin
+from l10n.admin import CountryCallingCodeOptions, CountryOptions
+from l10n.models import CountryCallingCode, Country
 from sso.accounts import models
 from sso.organisations import models as org_models
 from sso.organisations import admin as org_admin
 
-from sso.emails.models import Email, EmailForward, EmailAlias, GroupEmail, GroupEmailManager
-from sso.emails.admin import EmailAdmin, EmailAliasAdmin, EmailForwardAdmin, GroupEmailAdmin, GroupEmailManagerAdmin
-
-from l10n.admin import *
-
 
 class SSOAdminSite(admin.AdminSite):
-    # TODO: redirect to /accounts/login/ (see dwbn project)
-    login_form = EmailAuthenticationForm
-    login_template = 'accounts/login.html'
+    """
+    copy of django admin view with:
+    - redirecting to accounts:login instead of admin:login
+    """
+    def admin_view(self, view, cacheable=False):
+        def inner(request, *args, **kwargs):
+            if not self.has_permission(request):
+                if request.path == reverse('admin:logout', current_app=self.name):
+                    index_path = reverse('admin:index', current_app=self.name)
+                    return HttpResponseRedirect(index_path)
+                # Inner import to prevent django.contrib.admin (app) from
+                # importing django.contrib.auth.models.User (unrelated model).
+                from django.contrib.auth.views import redirect_to_login
+                return redirect_to_login(
+                    request.get_full_path(),
+                    reverse('accounts:login')
+                )
+            return view(request, *args, **kwargs)
+        if not cacheable:
+            inner = never_cache(inner)
+        # We add csrf_protect here so this function can be used as a utility
+        # function for any view, without having to repeat 'csrf_protect'.
+        if not getattr(view, 'csrf_exempt', False):
+            inner = csrf_protect(inner)
+        return update_wrapper(inner, view)
 
     def has_permission(self, request):
         """
