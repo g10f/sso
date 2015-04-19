a9aed32dea94a264c7cfe2eaf0f05c35847e838c
gunnar <gunnar@g10f.de>
2015-04-19 19:50:58 +0200

    cleanup

--- apps/sso/context_processors.py
+++ apps/sso/context_processors.py
@@ -16,7 +16,8 @@ def settings(request):
             'navigation_template': site_settings.SSO_NAVIGATION_TEMPLATE,
             'brand': site_settings.SSO_BRAND,
             'base_url': get_base_url(request),
-            'stylesheet': site_settings.SSO_STYLESHEET,
-            'stylesheet_less': site_settings.SSO_STYLE_LESS,
+            'stylesheet': 'css/%(style)s-%(version)s.css' % {'style': site_settings.SSO_STYLE, 'version': site_settings.SSO_STYLE_VERSION},
+            'stylesheet_less': 'less/%(style)s.less' % {'style': site_settings.SSO_STYLE},
+            'less': site_settings.SSO_LESS,
             'favicon': site_settings.SSO_FAVICON
             }
