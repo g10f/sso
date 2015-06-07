43810b9fdb6743071472386713b9e6de327623e0
gunnar <gunnar@g10f.de>
2015-06-07 22:45:26 +0200

    .

---
+++ apps/sso/oauth2/migrations/0003_auto_20150607_2245.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('oauth2', '0002_authorizationcode_otp_device'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='client',
+            name='scopes',
+            field=models.CharField(default=b'openid profile email', help_text="Allowed space-delimited access token scopes ('openid', 'profile', 'email', 'role', 'offline_access', 'address', 'phone', 'users', 'picture')", max_length=512, verbose_name='scopes', blank=True),
+        ),
+    ]
