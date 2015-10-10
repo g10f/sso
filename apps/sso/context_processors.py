5ed871d5206c02dded02398c23bcaf20decbdf6c
gunnar <gunnar@g10f.de>
2015-10-10 20:31:08 +0200

    added brand template dir for overwriting templates

--- apps/sso/context_processors.py
+++ apps/sso/context_processors.py
@@ -13,15 +13,14 @@ def get_base_url(request):
 
 def settings(request):
     
-    return {"global_navigation_template": site_settings.SSO_GLOBAL_NAVIGATION_TEMPLATE,
-            'navigation_template': site_settings.SSO_NAVIGATION_TEMPLATE,
-            'brand': site_settings.SSO_BRAND,
-            'base_url': get_base_url(request),
-            'stylesheet': 'css/%(style)s-%(version)s.css' % {'style': site_settings.SSO_STYLE, 'version': site_settings.SSO_STYLE_VERSION},
-            'stylesheet_less': 'less/%(style)s.less' % {'style': site_settings.SSO_STYLE},
-            'less': site_settings.SSO_LESS,
-            'favicon': site_settings.SSO_FAVICON,
-            'sso_app_uuid': site_settings.SSO_APP_UUID,
-            'registration_open': site_settings.REGISTRATION.get('OPEN', True),
-            'device_classes': get_device_classes()
-            }
+    return {
+        'brand': site_settings.SSO_BRAND,
+        'base_url': get_base_url(request),
+        'stylesheet': 'css/%(style)s-%(version)s.css' % {'style': site_settings.SSO_STYLE, 'version': site_settings.SSO_STYLE_VERSION},
+        'stylesheet_less': 'less/%(style)s.less' % {'style': site_settings.SSO_STYLE},
+        'less': site_settings.SSO_LESS,
+        'favicon': site_settings.SSO_FAVICON,
+        'sso_app_uuid': site_settings.SSO_APP_UUID,
+        'registration_open': site_settings.REGISTRATION.get('OPEN', True),
+        'device_classes': get_device_classes()
+        }
