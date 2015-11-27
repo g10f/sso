4e13e783f7303e65e6d9a49e9117c18396366241
gunnar <gunnar@g10f.de>
2015-11-27 09:40:07 +0100

    tabular forms javascript bug

--- apps/sso/views/generic.py
+++ apps/sso/views/generic.py
@@ -170,7 +170,7 @@ class FormsetsUpdateView(generic.UpdateView):
         
     @property
     def media(self):
-        js = ['formsets-1.1.js']
+        js = ['formsets-1.2.js']
         return forms.Media(js=[static('js/%s' % url) for url in js]) 
 
     @property
