2817fe2ce3b2ece3a14a545ccf081d89c7f7e460
gunnar <gunnar@g10f.de>
2017-05-02 18:48:24 +0200

    django 1.11

--- apps/sso/views/csrf.py
+++ apps/sso/views/csrf.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.template import Context
 from django.utils.translation import ugettext as _
 
 
 def csrf_failure(request, reason="", template='csrf_failure.html'):
     from django.middleware.csrf import REASON_NO_REFERER, REASON_NO_CSRF_COOKIE
-    c = Context({
+    c = {
         'title': _("Forbidden"),
         'main': _("CSRF verification failed. Request aborted."),
         'reason': reason,
         'no_referer': reason == REASON_NO_REFERER,
         'no_cookie': reason == REASON_NO_CSRF_COOKIE,
-    })
+    }
     return render(request, template, context=c, status=403)
