58d0d1cc6f162e3a07af9082f4fe5ed9eeae019c
gunnar <gunnar@g10f.de>
2018-05-20 01:55:49 +0200

    clear only 2 weeks old tokens

--- apps/sso/oauth2/management/commands/cleartokens.py
+++ apps/sso/oauth2/management/commands/cleartokens.py
@@ -1,16 +1,16 @@
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from ...models import AuthorizationCode, BearerToken 
+from ...models import AuthorizationCode, BearerToken
 
 
 class Command(BaseCommand):
     help = "Can be run as a cronjob or directly to clean out expired tokens."
 
     def handle(self, **options):
-        yesterday = timezone.now() - timedelta(0, 86400)
+        two_weeks = timezone.now() - timedelta(14)
         five_minutes_ago = timezone.now() - timedelta(0, 300)
         # AuthorizationCode are short living, only to create a bearer token
         AuthorizationCode.objects.filter(created_at__lt=five_minutes_ago).delete()
         # BearerToken are valid for one hour, but are stored for monitoring for 1 day
-        BearerToken.objects.filter(created_at__lt=yesterday).delete()
+        BearerToken.objects.filter(created_at__lt=two_weeks).delete()
