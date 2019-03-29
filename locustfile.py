820848cdf07b48a7d4d33867dfb83e4e4180dbc8
Scherf, Gunnar (K-FIT/32) <gunnar.scherf@volkswagen.de>
2019-03-29 11:14:43 +0100

    config update

--- locustfile.py
+++ locustfile.py
@@ -1,105 +1,136 @@
-import json
 import os
-from pyquery import PyQuery
-from urllib import urlencode
-from uritemplate import expand
+import re
+from base64 import b64encode
+from urllib.parse import urlencode, urlsplit, parse_qs
+
 from locust import HttpLocust, TaskSet, task
+from pyquery import PyQuery
 
-try:
-    from urllib.parse import urlsplit
-except ImportError:  # Python 2
-    from urlparse import urlsplit
+pattern = re.compile("^{(?P<env_var>.*)}$")
 
-OAUTH2_CLIENT = {
-    'host': 'http://localhost:3001',
-    'grant_type': 'authorization_code',
-    'scope': 'openid profile email',
-    'response_type': 'code',
-    'redirect_uri': 'urn:ietf:wg:oauth:2.0:oob',
-    'client_id': "31e873e25a614988815544cb00e66ba3",
-    'client_secret': "e0]GN+Rp@O3H}}CDnKxl",
-    'username': "test@g10f.de",
-    'password': ".h!~c.R-rq6e!WY#[rXZ"
+test_data = {
+    "g10f_user": {
+        "username": "{G10F_USER_NAME}",
+        "password": "{G10F_USER_PASSWORD}",
+    },
+    "g10f_oidc_web_client": {
+        "client_id": "{G10F_OIDC_WEB_CLIENT_ID}",
+        "client_secret": "{G10F_OIDC_WEB_CLIENT_SECRET}",
+        "redirect_uri": "http://localhost:8001/oauth2/login/",
+        "scope": "openid profile email",
+        'grant_type': 'authorization_code',
+        'response_type': 'code',
+    },
+    "idp": {
+        "oidc": {
+            "verify_tls": False,
+            "iss": "https://sso.elsapro.com",
+        }
+    },
+    "proxies": {
+        "http": "{http_proxy}",
+        "https": "{https_proxy}"
+    }
 }
 
-if "CLIENT_ID" in os.environ:
-    OAUTH2_CLIENT["client_id"] = os.environ['CLIENT_ID']
-if "USERNAME" in os.environ:
-    OAUTH2_CLIENT["username"] = os.environ['USERNAME']
-if "PASSWORD" in os.environ:
-    OAUTH2_CLIENT["password"] = os.environ['PASSWORD']
-if "CLIENT_SECRET" in os.environ:
-    OAUTH2_CLIENT["client_secret"] = os.environ['CLIENT_SECRET']
+
+def update_values_from_environment(data):
+    # iterate thru dict
+    for k, v in data.items():
+        if isinstance(v, dict):
+            # if value is a dict iterate
+            update_values_from_environment(v)
+        elif isinstance(v, str):
+            # find {NAME} patterns
+            m = pattern.match(v)
+            if m:
+                # update configuration
+                env_var = m.group('env_var')
+                if os.environ.get(env_var) is not None:
+                    data[k] = os.environ.get(env_var)
+
+
+update_values_from_environment(test_data)
 
 
 class UserBehavior(TaskSet):
-    @property
-    def authentication_uri(self):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.openid_configuration = {}
+        self.proxies = {}  # test_data['proxies']
+
+    def get_authentication_uri(self, client):
         query = {
-            'scope': OAUTH2_CLIENT['scope'],
-            'client_id': OAUTH2_CLIENT['client_id'],
+            'scope': client['scope'],
+            'client_id': client['client_id'],
             'state': 'locust_test',
-            'response_type': OAUTH2_CLIENT['response_type'],
-            'redirect_uri': OAUTH2_CLIENT['redirect_uri']
+            'response_type': client['response_type'],
+            'redirect_uri': client['redirect_uri']
         }
         return "%s?%s" % (self.openid_configuration['authorization_endpoint'], urlencode(query))
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
-        response = self.client.get('/.well-known/openid-configuration')
+        response = self.client.get('/.well-known/openid-configuration', proxies=self.proxies)
         self.openid_configuration = response.json()
 
     @task
     def login(self):
         # SSO login for installed apps to get an auth code
-        response = self.client.get(self.authentication_uri)
+        user = test_data['g10f_user']
+        client = test_data['g10f_oidc_web_client']
+        headers = {"referer": client['redirect_uri']}
+        response = self.client.get(self.get_authentication_uri(client), headers=headers, proxies=self.proxies,
+                                   name=urlsplit(self.openid_configuration['authorization_endpoint']).path)
         pq = PyQuery(response.content)
-        headers = {"referer": response.request.url}
         data = {
-            "username": OAUTH2_CLIENT['username'],
-            "password": OAUTH2_CLIENT['password'],
+            "username": user['username'],
+            "password": user['password'],
             "csrfmiddlewaretoken": pq("input[name='csrfmiddlewaretoken']").val(),
-            # "goji.csrf.Token": pq("input[name='goji.csrf.Token']").val(),
             "login_form_key": "login_form",
             "next": pq("input[name='next']").val()
         }
-        response = self.client.post(response.request.path_url, data=data, headers=headers)
-        pq = PyQuery(response.content)
-        code = pq("#code").val()
+        path_url = response.request.path_url
+        headers={"referer": response.request.url}
+        response = self.client.post(path_url, data=data, headers=headers,
+                                    proxies=self.proxies,
+                                    allow_redirects=False,
+                                    name=urlsplit(path_url).path)
+        response = self.client.get(response.next.url, headers=headers, proxies=self.proxies,
+                                   allow_redirects=False,
+                                   name=urlsplit(response.next.url).path)
+        query = parse_qs(urlsplit(response.next.url).query)
 
-        # user logout, because we have now a auth code to authenticate 
-        self.client.get(self.openid_configuration['end_session_endpoint'])
+        # user logout, because we have now a auth code to authenticate
+        self.client.get(self.openid_configuration['end_session_endpoint'], proxies=self.proxies)
 
-        # use the auth code to get a token
-        data = {
-            'grant_type': OAUTH2_CLIENT['grant_type'],
-            'client_id': OAUTH2_CLIENT['client_id'],
-            'client_secret': OAUTH2_CLIENT['client_secret'],
-            'code': code,
-            'redirect_uri': OAUTH2_CLIENT['redirect_uri']
-        }
-        # print(data)
         headers = {'content-type': 'application/x-www-form-urlencoded', 'Accept': 'application/json'}
-        response = self.client.post(self.openid_configuration['token_endpoint'], data=data, headers=headers)
-        # print(response.content)
+        client_id = client['client_id']
+        client_secret = client['client_secret']
+        data = {
+            'grant_type': 'authorization_code',
+            'code': query['code'][0],
+            'redirect_uri': client['redirect_uri']
+        }
+        # if code_verifier:
+        #    data['code_verifier'] = code_verifier
+        if client_secret:
+            auth = b"%s:%s" % (client_id.encode(), client_secret.encode())
+            headers['authorization'] = '%s %s' % ('Basic', b64encode(auth).decode("ascii"))
+
+        response = self.client.post(self.openid_configuration['token_endpoint'], data=data, headers=headers,
+                                    proxies=self.proxies)
+
         content = response.json()
-        # print(content)
-        # get userinfo
-        return
         headers = {
             'accept': 'application/json',
             'authorization': '%s %s' % (content['token_type'], content['access_token'])
         }
-        response = self.client.get(self.openid_configuration['userinfo_endpoint'], headers=headers)
-        # print(response.content)
-
-    # @task
-    def index(self):
-        self.client.get("/")
+        self.client.get(self.openid_configuration['userinfo_endpoint'], headers=headers, proxies=self.proxies)
 
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
     min_wait = 1000
     max_wait = 2000
-    host = OAUTH2_CLIENT['host']
+    host = "{0}://{1}".format(*urlsplit(test_data['idp']['oidc']['iss']))
