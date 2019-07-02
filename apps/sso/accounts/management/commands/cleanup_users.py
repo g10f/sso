015aea464edc3b397372d1a17f5d80933c2e4227
gunnar <gunnar@g10f.de>
2019-07-02 16:58:47 +0200

    added delete deactivated users function in cleanup task

--- apps/sso/accounts/management/commands/cleanup_users.py
+++ apps/sso/accounts/management/commands/cleanup_users.py
@@ -1,17 +1,17 @@
-import os
 import logging
-
+import os
 from datetime import timedelta
 
 import reversion
-from django.db.models.aggregates import Count
-from django.db.models.query_utils import Q
 
-from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+from django.db.models.aggregates import Count
+from django.db.models.query_utils import Q
 from django.utils.timezone import now
 from sso.accounts.models import User, RoleProfile
+from sso.registration.models import RegistrationManager
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +20,20 @@ class Command(BaseCommand):
     help = 'Cleanup Users'  # @ReservedAssignment
 
     def handle(self, *args, **options):
+        delete_deactivated_users()
         check_validation()
+
+
+def delete_deactivated_users():
+    with reversion.create_revision():
+        activation_expiration_date = RegistrationManager.activation_expiration_date()
+        reversion.set_comment("Deleting deactivated accounts in cleanup_users task")
+        q = Q(last_login__isnull=True) | Q(last_login__lte=activation_expiration_date)
+        count = 0
+        for user in User.objects.filter(q, is_active=False, last_modified__lte=activation_expiration_date):
+            user.delete()
+            count += 1
+        logger.debug("Deleted %s user(s)" % count)
 
 
 def check_validation():
@@ -34,8 +47,8 @@ def check_validation():
 
     # 1. Assign Guest Profile to expired user accounts
     has_already_guest_status = Q(application_roles=None) & Q(count_profiles=1) & Q(role_profiles=guest_profile)
-    expired_users = User.objects.annotate(count_profiles=Count('role_profiles')).\
-        filter(valid_until__lt=now()).\
+    expired_users = User.objects.annotate(count_profiles=Count('role_profiles')). \
+        filter(valid_until__lt=now()). \
         exclude(has_already_guest_status)
     if not settings.SSO_VALIDATION_PERIOD_IS_ACTIVE_FOR_ALL:
         expired_users = expired_users.filter(organisations__uses_user_activation=True)
@@ -51,7 +64,7 @@ def check_validation():
             logger.debug("%s" % expired_user)
 
     # 2. user with valid_until__isnull=True and a organisation which uses user activation will expire in 30 days
-    new_users = User.objects.annotate(count_profiles=Count('role_profiles'))\
+    new_users = User.objects.annotate(count_profiles=Count('role_profiles')) \
         .filter(is_active=True, valid_until__isnull=True, is_service=False, is_center=False)
     if not settings.SSO_VALIDATION_PERIOD_IS_ACTIVE_FOR_ALL:
         new_users = new_users.filter(organisations__uses_user_activation=True)
