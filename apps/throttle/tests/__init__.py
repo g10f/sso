adba0772bb2f5dacef078f3b3139ba37bc0c1ebe
gunnar <gunnar@g10f.de>
2019-08-17 16:22:34 +0200

    assertEquals is deprecated

--- apps/throttle/tests/__init__.py
+++ apps/throttle/tests/__init__.py
@@ -24,40 +24,40 @@ class ThrottleTest(test.TestCase):
         """
         Tests default usage
         """
-        self.assertEquals(200, self.request('test_default').status_code)
-        self.assertEquals(403, self.request('test_default').status_code)
+        self.assertEqual(200, self.request('test_default').status_code)
+        self.assertEqual(403, self.request('test_default').status_code)
 
     def test_method(self):
         """
         Tests that decorator applies to view for specified method and
         not applies for another
         """
-        self.assertEquals(200, self.request('test_method', method='GET').status_code)
-        self.assertEquals(403, self.request('test_method', method='GET').status_code)
-        self.assertEquals(200, self.request('test_method', method='POST').status_code)
+        self.assertEqual(200, self.request('test_method', method='GET').status_code)
+        self.assertEqual(403, self.request('test_method', method='GET').status_code)
+        self.assertEqual(200, self.request('test_method', method='POST').status_code)
 
     def test_response(self):
         """
         Tests custom response decorator argument
         """
-        self.assertEquals(200, self.request('test_response').status_code)
-        self.assertEquals(401, self.request('test_response').status_code)
-        self.assertEquals(True, b'Response' in self.request('test_response').content)
+        self.assertEqual(200, self.request('test_response').status_code)
+        self.assertEqual(401, self.request('test_response').status_code)
+        self.assertEqual(True, b'Response' in self.request('test_response').content)
 
     def test_response_callable(self):
         """
         Tests custom response decorator argument
         """
-        self.assertEquals(200, self.request('test_response_callable').status_code)
-        self.assertEquals(401, self.request('test_response_callable').status_code)
-        self.assertEquals(True, b'Request Response' in self.request('test_response_callable').content)
+        self.assertEqual(200, self.request('test_response_callable').status_code)
+        self.assertEqual(401, self.request('test_response_callable').status_code)
+        self.assertEqual(True, b'Request Response' in self.request('test_response_callable').content)
 
     def test_duration(self):
         """
         Tests custom duration
         """
-        self.assertEquals(200, self.request('test_duration').status_code)
-        self.assertEquals(200, self.request('test_duration').status_code)
+        self.assertEqual(200, self.request('test_duration').status_code)
+        self.assertEqual(200, self.request('test_duration').status_code)
 
 
 def index(request):
