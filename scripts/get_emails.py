7bfa6c7a084ee755f5055aa808c1d72ef766cdd0
Gunnar Scherf <mail@g10f.de>
2020-12-28 17:37:10 +0100

    cleanup

--- scripts/get_emails.py
+++ scripts/get_emails.py
@@ -8,7 +8,7 @@ from urllib import urlencode
 
 def get(conn, uri, access_token=None):
     """
-    HTTP GET request with an optional authorization header with the access_token 
+    HTTP GET request with an optional authorization header with the access_token
     """
     if access_token:
         headers = {'authorization': '%s %s' % (access_token['token_type'], access_token['access_token'])}
@@ -25,13 +25,13 @@ def get_json(conn, uri, access_token=None):
 
 def get_access_token_with_client_credentials(conn, client_id, client_secret):
     """
-    get the token endpoint from the well-known uri and 
+    get the token endpoint from the well-known uri and
     then authenticate with grant_type client_credentials
     """
     openid_configuration = get_json(conn, '/.well-known/openid-configuration')
     token_endpoint = openid_configuration['token_endpoint']
-    
-    body = urlencode({'grant_type': 'client_credentials'})  
+
+    body = urlencode({'grant_type': 'client_credentials'})
     auth = b"%s:%s" % (client_id, client_secret)
     headers = {'Content-Type': 'application/x-www-form-urlencoded', 'authorization': '%s %s' % ('Basic', b64encode(auth).decode("ascii"))}
     conn.request('POST', token_endpoint, headers=headers, body=body)
@@ -50,15 +50,15 @@ def main():
     parser.add_argument('client_secret')
     parser.add_argument('-d', '--host', default='sso.g10f.de')
     args = parser.parse_args()
-    
+
     conn = httplib.HTTPSConnection(args.host)
     # authentication
     access_token = get_access_token_with_client_credentials(conn, args.client_id, args.client_secret)
     # API request
     data = get(conn, '/api/emails.txt', access_token)
-    
+
     conn.close()
-    print data
+    print(data)
 
 
 if __name__ == "__main__":
