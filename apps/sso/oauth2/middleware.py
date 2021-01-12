70b5574115fe628e006ead26a31911c88acfa499
Gunnar Scherf <mail@g10f.de>
2021-01-12 11:21:13 +0100

    SESSION_COOKIE_SAMESITE = 'None'  instead of None

--- apps/sso/oauth2/middleware.py
+++ apps/sso/oauth2/middleware.py
@@ -1,19 +1,18 @@
-from django.utils.deprecation import MiddlewareMixin
+import logging
+
 from jwt import InvalidTokenError
-from django.utils.functional import SimpleLazyObject, empty
+
+from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.crypto import constant_time_compare
-from django.contrib.auth.models import AnonymousUser
-from django.conf import settings
-
+from django.utils.deprecation import MiddlewareMixin
+from django.utils.functional import SimpleLazyObject, empty
+from sso import auth as sso_auth
+from sso.auth import get_session_auth_hash
 from .crypt import loads_jwt
 from .models import Client
-from sso import auth as sso_auth
-
-
-import logging
-from sso.auth import get_session_auth_hash
 
 logger = logging.getLogger(__name__)
 
@@ -47,7 +46,7 @@ def get_auth_data_from_token(access_token):
         session_hash = data.get(sso_auth.HASH_SESSION_KEY)
         user = get_user_model().objects.get(uuid=data['sub'])
         session_hash_verified = session_hash and \
-            constant_time_compare(session_hash, get_session_auth_hash(user, client))
+                                constant_time_compare(session_hash, get_session_auth_hash(user, client))
 
         if not session_hash_verified:
             logger.warning('session_ hash verification failed. jwt data: %s', data)
@@ -101,8 +100,9 @@ def get_auth_data(request):
 
 class OAuthAuthenticationMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        assert hasattr(request, 'session'), "The Django authentication middleware requires session middleware to be installed. \
-        Edit your MIDDLEWARE_CLASSES setting to insert 'django.contrib.sessions.middleware.SessionMiddleware'."
+        assert hasattr(request, 'session'), "The Django authentication middleware requires session middleware to be " \
+                                            "installed. Edit your MIDDLEWARE_CLASSES setting to insert " \
+                                            "'django.contrib.sessions.middleware.SessionMiddleware'."
 
         request.user = SimpleLazyObject(lambda: get_auth_data(request)[0])
         request.client = SimpleLazyObject(lambda: get_auth_data(request)[1])
