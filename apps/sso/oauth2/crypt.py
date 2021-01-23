4efb0a5a4f6a0e41ad4e977e63132a66c8c7860c
Gunnar Scherf <mail@g10f.de>
2021-01-23 11:08:29 +0100

    keys rollover

--- apps/sso/oauth2/crypt.py
+++ apps/sso/oauth2/crypt.py
@@ -1,16 +1,13 @@
 import logging
 
 import time
-from jwt import decode, encode
+from jwt import decode, encode, InvalidSignatureError, get_unverified_header
 
 from django.conf import settings
-from django.utils.encoding import force_str
 
 logger = logging.getLogger(__name__)
 
 MAX_AGE = 3600  # one hour
-
-_algorithms = ["RS256", "HS256"]
 
 
 def make_jwt(payload, max_age=MAX_AGE, algorithm="RS256"):
@@ -26,27 +23,31 @@ def make_jwt(payload, max_age=MAX_AGE, algorithm="RS256"):
     if "exp" not in payload:
         payload["exp"] = int(time.time()) + max_age  # add  expired at time
 
-    if algorithm == "RS256":
-        key = settings.CERTS['default']['PRIVATE_KEY']
-    elif algorithm == "HS256":
-        key = settings.SECRET_KEY
-    else:
-        raise NotImplementedError('Algorithm %s not supported', algorithm)
-    bytes_string = encode(payload, key=key, algorithm=algorithm)
-    return force_str(bytes_string)
+    signing = settings.SIGNING[algorithm]
+    kid = signing['active']
+    key = signing['keys'][kid]['SECRET_KEY']
+    # TODO: add kid to headers
+    # return encode(payload, key=key, algorithm=algorithm, headers={"kid": kid})
+    return encode(payload, key=key, algorithm=algorithm)
 
 
 def loads_jwt(jwt, algorithm="RS256", verify=True, options=None):
-    """
-    Reverse of make_jwt(), raises InvalidTokenError if something fails.
-    """
-    if algorithm == "RS256":
-        key = settings.CERTS['default']['public_key']
-    elif algorithm == "HS256":
-        key = settings.SECRET_KEY
-    else:
-        raise NotImplementedError('Algorithm %s not supported', algorithm)
+    _algorythm_keys = {'RS256': 'public_key', 'HS256': 'SECRET_KEY'}
     if options is None:
         options = {"verify_aud": False, "require": ["exp", "iat"], "verify_exp": True, "verify_iat": True}
 
-    return decode(jwt, algorithms=[algorithm], key=key, verify=verify, options=options)
+    header = get_unverified_header(jwt)
+    if 'kid' in header:
+        key = settings.SIGNING[algorithm]['keys'][header['kid']][_algorythm_keys[algorithm]]
+        return decode(jwt, algorithms=[algorithm], key=key, verify=verify, options=options)
+
+    # else iterate over all keys
+    for value in settings.SIGNING[algorithm]['keys'].values():
+        try:
+            key = value[_algorythm_keys[algorithm]]
+            return decode(jwt, algorithms=[algorithm], key=key, verify=verify, options=options)
+        except InvalidSignatureError as e:
+            logger.debug(e)
+
+    logger.info(f"InvalidSignatureError for {jwt}")
+    raise InvalidSignatureError(jwt)
