f36c9d8060fb0a4e5eca329f29b17efabd2260fa
Gunnar Scherf <mail@g10f.de>
2021-03-23 22:07:23 +0100

    .

--- apps/sso/registration/sites.py
+++ apps/sso/registration/sites.py
@@ -22,24 +22,16 @@ class RegistrationSite(object):
     def get_urls(self):
         urlpatterns = [
             # registration
-            path('register/validate/complete/',
-                 TemplateView.as_view(template_name='registration/validation_complete.html'),
-                 name='validation_complete'),
-            path('register/validate/complete2/',
-                 TemplateView.as_view(template_name='registration/validation_complete2.html'),
-                 name='validation_complete2'),
+            path('register/validate/complete/', TemplateView.as_view(template_name='registration/validation_complete.html'), name='validation_complete'),
+            path('register/validate/complete2/', TemplateView.as_view(template_name='registration/validation_complete2.html'), name='validation_complete2'),
             path('register/validate/<slug:uidb64>/<slug:token>/', validation_confirm, name='validation_confirm'),
             path('register/', UserSelfRegistrationFormPreview(self.form_cls), name='registration_register'),
-            path('register/done/', TemplateView.as_view(template_name='registration/registration_done.html'),
-                 name='registration_done'),
-            path('register/closed/', TemplateView.as_view(template_name='registration/registration_closed.html'),
-                 name='registration_disallowed'),
+            path('register/done/', TemplateView.as_view(template_name='registration/registration_done.html'), name='registration_done'),
+            path('register/closed/', TemplateView.as_view(template_name='registration/registration_closed.html'), name='registration_disallowed'),
             path('registrations/', UserRegistrationList.as_view(), name='user_registration_list'),
             path('registrations/<int:pk>/', update_user_registration, name="update_user_registration"),
-            path('registrations/delete/<int:pk>/', UserRegistrationDeleteView.as_view(),
-                 name="delete_user_registration"),
-            path('registrations/process/<int:pk>/<slug:action>/', RegistrationSendMailFormView.as_view(),
-                 name="process_user_registration"),
+            path('registrations/delete/<int:pk>/', UserRegistrationDeleteView.as_view(), name="delete_user_registration"),
+            path('registrations/process/<int:pk>/<slug:action>/', RegistrationSendMailFormView.as_view(), name="process_user_registration"),
         ]
         return urlpatterns
 
