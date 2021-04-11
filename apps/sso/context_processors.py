47e41ee755bf1c77542df2e13f80b25e19818e2a
Gunnar Scherf <mail@g10f.de>
2021-04-11 12:21:02 +0200

    new sidebar nav

--- apps/sso/context_processors.py
+++ apps/sso/context_processors.py
@@ -1,9 +1,7 @@
 import logging
 
-from django.apps import apps
-
 from django.conf import settings as site_settings
-from sso.auth.utils import get_device_classes
+from sso.sidebar import sidebar
 from sso.utils.url import get_base_url
 
 log = logging.getLogger(__name__)
@@ -13,16 +11,10 @@ def settings(request):
     return {
         'brand': site_settings.SSO_BRAND,
         'base_url': get_base_url(request),
-        'stylesheet': 'css/%(style)s-%(version)s.css' % {'style': site_settings.SSO_STYLE,
-                                                         'version': site_settings.SSO_STYLE_VERSION},
+        'stylesheet': 'css/%(style)s-%(version)s.css' % {'style': site_settings.SSO_STYLE, 'version': site_settings.SSO_STYLE_VERSION},
         'stylesheet_less': 'less/%(style)s.less' % {'style': site_settings.SSO_STYLE},
         'sso_app_uuid': site_settings.SSO_APP_UUID,
         'registration_open': site_settings.REGISTRATION.get('OPEN', True),
-        'device_classes': get_device_classes(),
-        'admin_only_2f': site_settings.SSO_ADMIN_ONLY_2F,
-        'email_management': site_settings.SSO_ORGANISATION_EMAIL_MANAGEMENT,
-        'region_management': site_settings.SSO_REGION_MANAGEMENT,
-        'country_management': site_settings.SSO_COUNTRY_MANAGEMENT,
         'data_protection_uri': site_settings.SSO_DATA_PROTECTION_URI,
-        'sso_access_requests_is_installed': apps.is_installed('sso.access_requests')
+        'sidebar': sidebar(request)
     }
