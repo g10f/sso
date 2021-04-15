5d18d0a6169e6bb9beb755201c657346bdf53408
Gunnar Scherf <mail@g10f.de>
2021-04-15 12:38:26 +0200

    cache jsi18n page

--- apps/sso/urls.py
+++ apps/sso/urls.py
@@ -1,7 +1,11 @@
+import sso
 from django.apps import apps
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path, register_converter
+from django.utils import timezone
+from django.views.decorators.cache import cache_page
+from django.views.decorators.http import last_modified
 from django.views.generic import RedirectView, TemplateView
 from django.views.i18n import JavaScriptCatalog
 from smart_selects.views import filterchain
@@ -15,11 +19,14 @@ from sso.views import home
 registration_site = RegistrationSite(form_cls=get_default_user_self_registration_form_class())
 
 register_converter(UUIDConverter, 'uuid')
+last_modified_date = timezone.now()
 
 urlpatterns = [
     path('', home, name='home'),
     path('.well-known/openid-configuration', OpenidConfigurationView.as_view(), name='openid-configuration'),
-    path('jsi18n/', JavaScriptCatalog.as_view(packages=['sso']), name='jsi18n'),
+    path('jsi18n/',
+         cache_page(60 * 60 * 24, key_prefix='js18n-%s' % sso.__version__)(last_modified(lambda req, **kw: last_modified_date)(JavaScriptCatalog.as_view())),
+         name='jsi18n'),
     path('admin/', sso_admin_site.urls),
     path('privacy/', TemplateView.as_view(template_name="privacy.html"), name='privacy'),
     path('about/', RedirectView.as_view(url=settings.SSO_ABOUT, permanent=False), name='about'),
