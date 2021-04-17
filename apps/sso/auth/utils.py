4a2dae0ad1b60f1698c15b5847025b1ca34eb6f2
Gunnar Scherf <mail@g10f.de>
2021-04-17 12:17:56 +0200

    menue update and form for changing security device

--- apps/sso/auth/utils.py
+++ apps/sso/auth/utils.py
@@ -60,24 +60,6 @@ def totp_digits():
     return getattr(settings, 'TWO_FACTOR_TOTP_DIGITS', 6)
 
 
-def class_view_decorator(function_decorator):
-    """
-    Converts a function based decorator into a class based decorator usable
-    on class based Views.
-
-    Can't subclass the `View` as it breaks inheritance (super in particular),
-    so we monkey-patch instead.
-
-    From: http://stackoverflow.com/a/8429311/58107
-    """
-
-    def simple_decorator(View):
-        View.dispatch = method_decorator(function_decorator)(View.dispatch)
-        return View
-
-    return simple_decorator
-
-
 def default_device(user, is_otp_enabled=True):
     from sso.auth.models import Device
     try:
