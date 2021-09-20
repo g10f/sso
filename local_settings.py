625ac7af861c9fabc3e8458b795d3b0db6b7095f
Gunnar Scherf <mail@g10f.de>
2021-09-20 21:01:15 +0200

    docker-compose with memcached

--- local_settings.py
+++ local_settings.py
@@ -2,3 +2,12 @@
 # to configure the sso
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'sso.cache.backends.SSOCache',
+#         'LOCATION': 'cache:11211',
+#         'TIMEOUT': 300,
+#         'KEY_PREFIX': 'sso'
+#     }
+# }
