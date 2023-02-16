2bf479cb8ea9cdfef17eac769ab013a90c04c758
Gunnar Scherf <mail@gunnar-scherf.de>
2023-02-16 21:48:47 +0100

    translation

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tag=$(python apps/version.py)
+tag=$(python3 apps/version.py)
 DOCKER_BUILDKIT=1
 # docker buildx create --use
 docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/g10f/sso:$tag -t g10f/sso:latest --push .
