00c4c9b3697b698e97344d7a9402b70313ff3693
Gunnar Scherf <mail@gunnar-scherf.de>
2023-08-10 17:29:28 +0200

    bootstrap 5.3.1

--- gulp/buildStyles.js
+++ gulp/buildStyles.js
@@ -6,14 +6,14 @@ const rename = require("gulp-rename");
 function buildStyles() {
     return gulp.src(global.config.srcCss)
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({includePaths: 'node_modules'}).on('error', sass.logError))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(global.config.buildCss))
 }
 
 function buildMinStyles() {
     return gulp.src(global.config.srcCss)
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({includePaths: 'node_modules', outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({extname: '.min.css'}))
         .pipe(gulp.dest(global.config.buildCss))
 }
