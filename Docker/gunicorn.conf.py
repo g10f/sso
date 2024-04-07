6b3a27bbb129ca6858b6d882a5ef080c70df869a
Gunnar Scherf <mail@gunnar-scherf.de>
2024-04-07 13:02:12 +0200

    config workers with GUNICORN_CMD_ARGS or WEB_CONCURRENCY

--- Docker/gunicorn.conf.py
+++ Docker/gunicorn.conf.py
@@ -1,5 +1,5 @@
-import multiprocessing
+# import multiprocessing
 secure_scheme_headers = {'X-FORWARDED-SSL': 'on', 'X-FORWARDED-PROTO': 'https'}
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
 bind = "0.0.0.0:8000"
 forwarded_allow_ips = '*'
