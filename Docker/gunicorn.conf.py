7ad437b12ab048ab3d0a76274647a3decb87943e
Gunnar Scherf <mail@gunnar-scherf.de>
2024-04-07 13:42:15 +0200

    only set secure_scheme_headers in gunicorn.conf.py

--- Docker/gunicorn.conf.py
+++ Docker/gunicorn.conf.py
@@ -1,5 +1,1 @@
-# import multiprocessing
 secure_scheme_headers = {'X-FORWARDED-SSL': 'on', 'X-FORWARDED-PROTO': 'https'}
-# workers = multiprocessing.cpu_count() * 2 + 1
-# bind = "0.0.0.0:8000"
-forwarded_allow_ips = '*'
