bc6c15b7e462266eec6a6cfea703939bb04554cc
Gunnar Scherf <mail@gunnar-scherf.de>
2025-01-07 21:03:44 +0100

    spelling

--- apps/sso/sidebar.py
+++ apps/sso/sidebar.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext_lazy as _
 
 
 def sidebar(request):
-    def set_active_item(menue):
-        # iterate menue and find the active menue item
-        # if the item is inside an submenue, the submeue is expanded and marked active
-        for item in menue:
-            if 'submenue' in item:
-                if set_active_item(item['submenue']):
+    def set_active_item(menu):
+        # iterate menu and find the active menu item
+        # if the item is inside a submenu, the submenu  is expanded and marked active
+        for item in menu:
+            if 'submenu' in item:
+                if set_active_item(item['submenu']):
                     item['active'] = True
                     item['expanded'] = True
                     return True
@@ -18,23 +18,23 @@ def sidebar(request):
                 item['active'] = True
                 return True
 
-    def reverse_url(menue):
-        # reverse the 1. item in from view_names and add a 'url' attribute for creating
-        # the link in the menue. Could also be done in the view
-        for item in menue:
-            if 'submenue' in item:
-                reverse_url(item['submenue'])
+    def reverse_url(menu):
+        # reverse the 1. item in from view_names and add an 'url' attribute for creating
+        # the link in the menu. Could also be done in the view
+        for item in menu:
+            if 'submenu' in item:
+                reverse_url(item['submenu'])
             else:
                 item['url'] = reverse(item['view_names'][0])
 
-    def append_submenue(menue):
-        if len(menue['submenue']) == 1:
-            sidebar_nav.append(menue['submenue'][0])
-        elif len(menue['submenue']) > 1:
-            sidebar_nav.append(menue)
+    def append_submenu(menu):
+        if len(menu['submenu']) == 1:
+            sidebar_nav.append(menu['submenu'][0])
+        elif len(menu['submenu']) > 1:
+            sidebar_nav.append(menu)
 
     user = request.user
-    # Main menue
+    # Main menu
     sidebar_nav = []
 
     # for 404 response request.resolver_match is None
@@ -44,32 +44,32 @@ def sidebar(request):
     if user.is_staff:
         sidebar_nav.append({'view_names': ['admin:index'], 'icon': 'wrench', 'title': _('Administration')})
 
-    # Application submenue
+    # Application submenu
     ######################
     application_list = {'view_names': ['accounts:application_list', 'accounts:application_update', 'accounts:application_detail', 'accounts:client_update', 'accounts:client_add'],
                         'icon': 'list-task', 'title': _('Applications')}
     application_create = {'view_names': ['accounts:application_add'], 'icon': 'plus', 'title': _('Add Application')}
 
-    application_submenue = {'name': 'applications', 'icon': 'grid-3x3-gap-fill', 'title': _('Applications'), 'submenue': []}
+    application_submenu = {'name': 'applications', 'icon': 'grid-3x3-gap-fill', 'title': _('Applications'), 'submenu': []}
     if user.has_perm('accounts.view_application'):
-        application_submenue['submenue'].append(application_list)
+        application_submenu['submenu'].append(application_list)
     if user.has_perm('accounts.add_application'):
-        application_submenue['submenue'].append(application_create)
+        application_submenu['submenu'].append(application_create)
 
-    # E-Mail submenue
+    # E-Mail submenu
     #################
     email_list = {'view_names': ['emails:groupemail_list', 'emails:groupemail_detail', 'emails:groupemail_update', 'emails:emailforward_create',
                                  'emails:emailforward_confirm_delete'],
                   'icon': 'list-task', 'title': _('Emails')}
     email_create = {'view_names': ['emails:groupemail_create'], 'icon': 'plus', 'title': _('Add Email')}
 
-    email_submenue = {'name': 'emails', 'icon': 'envelope', 'title': _('E-Mails'), 'submenue': []}
+    email_submenu = {'name': 'emails', 'icon': 'envelope', 'title': _('E-Mails'), 'submenu': []}
     if settings.SSO_ORGANISATION_EMAIL_MANAGEMENT and user.is_groupemail_admin:
-        email_submenue['submenue'].append(email_list)
+        email_submenu['submenu'].append(email_list)
     if settings.SSO_ORGANISATION_EMAIL_MANAGEMENT and user.has_perm('emails.add_groupemail'):
-        email_submenue['submenue'].append(email_create)
+        email_submenu['submenu'].append(email_create)
 
-    # Organisations submenue
+    # Organisations submenu
     ########################
     country_list = {'view_names': ['organisations:organisationcountry_list', 'organisations:organisationcountry_update',
                                    'organisations:organisationcountry_detail'], 'icon': 'list-task', 'title': _('Countries')}
@@ -82,23 +82,23 @@ def sidebar(request):
     region_create = {'view_names': ['organisations:adminregion_create'], 'icon': 'plus', 'title': _('Add Region')}
     org_create = {'view_names': ['organisations:organisation_create'], 'icon': 'plus', 'title': _('Add Organisation')}
     my_org = {'view_names': ['organisations:my_organisation_detail'], 'icon': 'house', 'title': _('My Organisation')}
-    orgs_submenue = {'name': 'organisations', 'icon': 'house', 'title': _('Organisations'), 'submenue': []}
+    orgs_submenu = {'name': 'organisations', 'icon': 'house', 'title': _('Organisations'), 'submenu': []}
     if user.organisations.exists:
-        orgs_submenue['submenue'].append(my_org)
+        orgs_submenu['submenu'].append(my_org)
     if settings.SSO_COUNTRY_MANAGEMENT and user.is_authenticated:
-        orgs_submenue['submenue'].append(country_list)
+        orgs_submenu['submenu'].append(country_list)
     if settings.SSO_REGION_MANAGEMENT and user.has_perm('organisations.change_adminregion'):
-        orgs_submenue['submenue'].append(region_list)
+        orgs_submenu['submenu'].append(region_list)
     if user.is_authenticated:
-        orgs_submenue['submenue'].append(org_list)
+        orgs_submenu['submenu'].append(org_list)
     if settings.SSO_COUNTRY_MANAGEMENT and user.has_perm('organisations.add_organisationcountry') and user.get_administrable_associations():
-        orgs_submenue['submenue'].append(country_create)
+        orgs_submenu['submenu'].append(country_create)
     if settings.SSO_REGION_MANAGEMENT and user.has_perm('organisations.add_adminregion'):
-        orgs_submenue['submenue'].append(region_create)
+        orgs_submenu['submenu'].append(region_create)
     if user.has_perm('organisations.add_organisation'):
-        orgs_submenue['submenue'].append(org_create)
+        orgs_submenu['submenu'].append(org_create)
 
-    # Accounts submenue
+    # Accounts submenu
     ###################
     no_reg = user.get_count_of_registrationprofiles()
     no_orgchg = user.get_count_of_organisationchanges()
@@ -115,19 +115,19 @@ def sidebar(request):
                        'icon': 'folder-plus', 'title': _('Access Requests'), 'badge': no_ext}
     user_create = {'view_names': ['accounts:add_user'], 'icon': 'person-plus', 'title': _('Add User')}
 
-    accounts_submenue = {'name': 'accounts', 'icon': 'people', 'title': _('Accounts'), 'submenue': []}
+    accounts_submenu = {'name': 'accounts', 'icon': 'people', 'title': _('Accounts'), 'submenu': []}
     if (settings.REGISTRATION.get('OPEN', True) or no_reg > 0) and user.has_perm('registration.change_registrationprofile'):
-        accounts_submenue['submenue'].append(registration_list)
+        accounts_submenu['submenu'].append(registration_list)
     if user.is_user_admin:
-        accounts_submenue['submenue'].append(user_list)
+        accounts_submenu['submenu'].append(user_list)
     if user.is_app_user_admin():
-        accounts_submenue['submenue'].append(roles_list)
+        accounts_submenu['submenu'].append(roles_list)
     if user.has_perm('accounts.change_user'):
-        accounts_submenue['submenue'].append(org_changes_list)
+        accounts_submenu['submenu'].append(org_changes_list)
     if apps.is_installed('sso.access_requests') and user.has_perm('accounts.change_user'):
-        accounts_submenue['submenue'].append(access_req_list)
+        accounts_submenu['submenu'].append(access_req_list)
     if user.has_perms(['accounts.change_user', 'accounts.add_user']):
-        accounts_submenue['submenue'].append(user_create)
+        accounts_submenu['submenu'].append(user_create)
 
     # my account items
     ##################
@@ -138,19 +138,19 @@ def sidebar(request):
     my_security = {'view_names': ['auth:mfa-detail', 'auth:mfa-update', 'auth:totp_add_device', 'auth:u2f_add_device'],
                    'icon': 'shield-lock', 'title': _('Security')}
 
-    my_data_submenue = {'name': 'my-data', 'icon': 'person', 'title': _('Personal Data'), 'submenue': [my_account, my_password]}
+    my_data_submenu = {'name': 'my-data', 'icon': 'person', 'title': _('Personal Data'), 'submenu': [my_account, my_password]}
 
     if not user.is_center:
-        my_data_submenue['submenue'].append(my_emails)
+        my_data_submenu['submenu'].append(my_emails)
     if user.is_mfa_enabled:
-        my_data_submenue['submenue'].append(my_security)
+        my_data_submenu['submenu'].append(my_security)
 
     if user.has_perm('accounts.view_application'):
-        append_submenue(application_submenue)
-    append_submenue(email_submenue)
-    append_submenue(orgs_submenue)
-    append_submenue(accounts_submenue)
-    append_submenue(my_data_submenue)
+        append_submenu(application_submenu)
+    append_submenu(email_submenu)
+    append_submenu(orgs_submenu)
+    append_submenu(accounts_submenu)
+    append_submenu(my_data_submenu)
 
     set_active_item(sidebar_nav)
     reverse_url(sidebar_nav)
