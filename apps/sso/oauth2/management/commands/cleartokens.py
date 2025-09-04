3831e6818859a76c4a4720d5f517ae4e8c8b7d9e
Gunnar Scherf <mail@gunnar-scherf.de>
2025-09-04 11:39:52 +0200

    bootstrap 5.3.8 configurable refresh token max age

--- apps/sso/oauth2/management/commands/cleartokens.py
+++ apps/sso/oauth2/management/commands/cleartokens.py
@@ -1,4 +1,6 @@
 from datetime import timedelta
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from ...models import AuthorizationCode, BearerToken
@@ -8,9 +10,11 @@ class Command(BaseCommand):
     help = "Can be run as a cronjob or directly to clean out expired tokens."
 
     def handle(self, **options):
-        two_weeks = timezone.now() - timedelta(14)
+        refresh_token_life_time = timezone.now() - timedelta(seconds=settings.SSO_REFRESH_TOKEN_AGE)
         five_minutes_ago = timezone.now() - timedelta(0, 300)
         # AuthorizationCode are short living, only to create a bearer token
         AuthorizationCode.objects.filter(created_at__lt=five_minutes_ago).delete()
-        # BearerToken are valid for one hour, but are stored for monitoring for 1 day
-        BearerToken.objects.filter(created_at__lt=two_weeks).delete()
+        # BearerToken are valid for one hour, but are stored for refresh tokens.
+        # Refresh Tokens are valid for REFRESH_TOKEN_LIFE_TIME days and are deleted
+        # automatically when they BearerToken are deleted.
+        BearerToken.objects.filter(created_at__lt=refresh_token_life_time).delete()
